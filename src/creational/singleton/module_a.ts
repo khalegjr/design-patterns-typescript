69f2b7cfcf606429b2d30f139c757130211542b8
Khaled <khalegjr@gmail.com>
2022-07-06 21:22:38 -0300

    refac: usando geração de getters e setter do ts

--- src/creational/singleton/module_a.ts
+++ src/creational/singleton/module_a.ts
@@ -1,6 +1,6 @@
 import { MyDatabaseClassic } from './db/my-database-classic';
 
-const myDatabaseClassic = MyDatabaseClassic.getInstance();
+const myDatabaseClassic = MyDatabaseClassic.instance;
 myDatabaseClassic.add({ name: 'Khaled', age: 39 });
 myDatabaseClassic.add({ name: 'Camila', age: 34 });
 myDatabaseClassic.add({ name: 'Lilian', age: 30 });
