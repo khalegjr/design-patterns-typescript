c7bc8260f699e73272715262d7b76f11212f0d87
Khaled <khalegjr@gmail.com>
2022-07-08 16:19:57 -0300

    Refact to the builder designer pattern

--- src/creational/Builder/index.ts
+++ src/creational/Builder/index.ts
@@ -1,11 +1,18 @@
-import { MealBox } from './classes/meal-box';
-import { Beans, Meat, Rice } from './classes/meals';
+import { VeganDishBuilder } from './classes/vegan-dish-builder';
+import { MainDishBuilder } from './classes/main-dish-builder';
 
-const rice = new Rice('Arroz', 5);
-const beans = new Beans('Feijão', 8);
-const meat = new Meat('Carne', 20);
-const mealBox = new MealBox();
+const mainDishBuilder = new MainDishBuilder();
 
-mealBox.add(rice, beans, meat);
-console.log(mealBox);
-console.log(mealBox.getPrice());
+mainDishBuilder.makeMeal().makeDessert();
+console.log(mainDishBuilder.getMeal());
+console.log(mainDishBuilder.getPrice());
+
+mainDishBuilder.reset();
+
+const meal2 = mainDishBuilder.makeBeverage().getMeal();
+console.log(meal2);
+
+const veganDishBuilder = new VeganDishBuilder();
+const veganMeal = veganDishBuilder.makeMeal().getMeal();
+console.log(veganMeal);
+console.log(veganMeal.getPrice());
